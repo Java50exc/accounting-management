c8f116b34c8d9c20c0e7340fd12696e40a1f8eaf
allanteone <allanteone@gmail.com>
2024-03-28 09:07:36 +0200

    hw_82 adds model and dto

--- src/main/java/telran/probes/dto/AccountDto.java
+++ src/main/java/telran/probes/dto/AccountDto.java
@@ -1,5 +1,10 @@
 package telran.probes.dto;
 
-public record AccountDto(String email, String password, String[] roles) {
+import jakarta.validation.constraints.*;
+import static telran.probes.api.AccountingValidationErrorMessages.*;
+
+public record AccountDto(@Email(message = WRONG_EMAIL_FORMAT) @NotBlank(message = EMPTY_EMAIL) String email, 
+		@Size(min = PASSWORD_MIN_LENGTH, message = WRONG_PASSWORD_LENGTH) @Pattern(regexp = PASSWORD_REGEXP, message = PASSWORD_WHITESPACES) String password, 
+		@NotEmpty(message = EMPTY_ROLES) String[] roles) {
 
 }
--- src/main/java/telran/probes/model/Account.java
+++ src/main/java/telran/probes/model/Account.java
@@ -3,11 +3,28 @@ package telran.probes.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("accounts")
+import lombok.*;
+import telran.probes.dto.AccountDto;
+
+@Document(collection = "${app.accounts.collection.name}")
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Account {
 	@Id
 	String email;
 	String hashPassword;
 	String[] roles;
+	
+	public AccountDto toDto() {
+		return new AccountDto(email, passToAsterisk(hashPassword), roles);
+	}
+	
+	
+	private String passToAsterisk(String hashPassword) {
+		return "*".repeat(hashPassword.length());
+	}
 
 }
