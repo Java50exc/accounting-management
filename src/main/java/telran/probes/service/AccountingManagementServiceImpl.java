c703334076cc645db46c8cfbc42772ffd2c1b699
allanteone <allanteone@gmail.com>
2024-03-28 09:09:13 +0200

    hw_82 adds service implementation

---
+++ src/main/java/telran/probes/service/AccountingManagementServiceImpl.java
@@ -0,0 +1,50 @@
+package telran.probes.service;
+
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.*;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import telran.probes.dto.AccountDto;
+import telran.probes.exceptions.*;
+import telran.probes.model.Account;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AccountingManagementServiceImpl implements AccountingManagementService {
+	final MongoTemplate mongoTemplate;
+
+	@Override
+	public AccountDto addAccount(AccountDto account) {
+		Account accountDoc = Account.builder().email(account.email()).hashPassword(account.password())
+				.roles(account.roles()).build();
+
+		try {
+			mongoTemplate.insert(accountDoc);
+			log.debug("Service: addAccount: Account {} succesfully saved to db", accountDoc);
+		} catch (DuplicateKeyException e) {
+			log.error("Service: addAccount: Account with email {} already exists", accountDoc.getEmail());
+			throw new AccountIllegalStateException();
+		}
+		AccountDto accountRes = accountDoc.toDto();
+		log.trace("Service: addAccount: returns dto {}", accountRes);
+		return accountRes;
+	}
+
+	@Override
+	public AccountDto removeAccount(String email) {
+		Account account = mongoTemplate.findAndRemove(new Query(Criteria.where("email").is(email)), Account.class);
+		
+		if (account == null) {
+			log.error("Service: removeAccount: Account with email {} not found", email);
+			throw new AccountNotFoundException();
+		}
+		log.debug("Service: removeAccount: Account with email {} has been removed", email);
+		AccountDto accountRes = account.toDto();
+		log.trace("Service: removeAccount: returns dto {}", accountRes);
+		return accountRes;
+	}
+
+}
