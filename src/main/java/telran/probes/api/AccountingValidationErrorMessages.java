3cc42a3aac6632bbee3cb5f5cadd02713a3d00d6
allanteone <allanteone@gmail.com>
2024-03-28 09:08:48 +0200

    hw_82 adds error constants

---
+++ src/main/java/telran/probes/api/AccountingValidationErrorMessages.java
@@ -0,0 +1,12 @@
+package telran.probes.api;
+
+public interface AccountingValidationErrorMessages {
+	int PASSWORD_MIN_LENGTH = 8;
+	String PASSWORD_REGEXP = "^\\S*$";
+	String WRONG_EMAIL_FORMAT = "Wrong email format";
+	String EMPTY_EMAIL = "Email must not be empty";
+	String WRONG_PASSWORD_LENGTH = "Password length must at least " + PASSWORD_MIN_LENGTH + " characters";
+	String PASSWORD_WHITESPACES = "Password cannot include whitespaces";
+	String EMPTY_ROLES = "Roles list must not be empty";
+
+}
---
+++ src/main/java/telran/probes/api/ServiceExceptionMessages.java
@@ -0,0 +1,7 @@
+package telran.probes.api;
+
+public interface ServiceExceptionMessages {
+	String ACCOUNT_ALREADY_EXISTS = "Account with given email already exists";
+	String ACCOUNT_NOT_FOUND = "Account with a given email not exists";
+
+}
