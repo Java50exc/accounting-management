e236d5623903d45650bbf9c5c4b71e29bffa1501
allanteone <allanteone@gmail.com>
2024-03-31 00:49:52 +0300

    hw_83 updates exception messages

--- src/main/java/telran/probes/api/ServiceExceptionMessages.java
+++ src/main/java/telran/probes/api/ServiceExceptionMessages.java
@@ -3,5 +3,8 @@ package telran.probes.api;
 public interface ServiceExceptionMessages {
 	String ACCOUNT_ALREADY_EXISTS = "Account with given email already exists";
 	String ACCOUNT_NOT_FOUND = "Account with a given email not exists";
+	
+	String UPDATE_ANOTHER_ACCOUNT_PASSWORD = "Only current authenticated user may update its password";
+	String REMOVE_ANOTHER_ACCOUNT_PASSWORD = "Only current authenticated user may remove his account";
 
 }
