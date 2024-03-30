9e11ca8a58eea1ccb32bd52abc5b414f03705224
allanteone <allanteone@gmail.com>
2024-03-31 00:49:06 +0300

    hw_83 updates controller

--- src/main/java/telran/probes/controller/AccountingManagementController.java
+++ src/main/java/telran/probes/controller/AccountingManagementController.java
@@ -6,26 +6,33 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import telran.probes.dto.AccountDto;
+import telran.probes.dto.*;
 import telran.probes.service.AccountingManagementService;
 
 @RestController
+@RequestMapping("${app.accounts.api.path}")
 @RequiredArgsConstructor
 @Slf4j
 public class AccountingManagementController {
 	final AccountingManagementService accountService;
 	
-	
-	@PostMapping("${app.accounts.api.path}")
+	@PostMapping
 	AccountDto addAccount(@RequestBody @Valid AccountDto account) {
-		log.debug("Controller: addAccount received {}", account);
+		log.debug("AccountingManagementController: addAccount: received {}", account);
 		return accountService.addAccount(account);
 	}
 	
-	@DeleteMapping("${app.accounts.api.path}" + "/{email}")
+	@DeleteMapping("{email}")
 	AccountDto removeAccount(@PathVariable("email") @Email(message = WRONG_EMAIL_FORMAT) @NotEmpty(message = EMPTY_EMAIL_MSG) String email) {
-		log.debug("Controller: removeAccount received {}", email);
+		log.debug("AccountingManagementController: removeAccount: received {}", email);
 		return accountService.removeAccount(email);
+	}
+	
+	@PutMapping
+	PasswordUpdateData updatePassword(@RequestBody @Valid PasswordUpdateData accountUpdate) {
+		log.debug("AccountingManagementController: addAccount: received {}", accountUpdate);
+		accountService.updatePassword(accountUpdate.email(), accountUpdate.newPassword());
+		return accountUpdate;
 	}
 
 }
